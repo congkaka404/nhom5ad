ea2142529dff79cc367417cb5924fd31ae40b33c
thanhhere <tp2995392@gmail.com>
2025-08-01 12:50:36 +0700

    fix user

--- app/src/main/java/com/example/sqlite0/models/User.java
+++ app/src/main/java/com/example/sqlite0/models/User.java
@@ -3,13 +3,13 @@ package com.example.sqlite0.models;
 public class User {
     private int id;
     private String username;
-    private String X;
+    private String email;
     private String password;
 
     public User(int id, String username, String email, String password) {
         this.id = id;
         this.username = username;
-
+        this.email = email;
         this.password = password;
     }
 
